649fc6bcf5bc7a51550e57102fb989129b81d52e
James Cuzella <james.cuzella@lyraphase.com>
2021-09-17 10:17:00 -0600

    Set include-system-site-packages = true & Move requirements.txt to build-aux/jupyter-requirements.txt

--- bin/venv_jupyter_kernel.sh
+++ bin/venv_jupyter_kernel.sh
@@ -3,7 +3,11 @@
 
 # Assume user has mkvirtualenv
 [ -f ~/.virtualenvs/circalizer/bin/activate ] || mkvirtualenv circalizer
+if ! grep -qE '^include-system-site-packages\s+=\s+true$' ~/.virtualenvs/circalizer/pyvenv.cfg
+then
+	sed -i -e 's/^include-system-site-packages[[:space:]]*=[[:space:]]*.*$/include-system-site-packages = true/g' ~/.virtualenvs/circalizer/pyvenv.cfg
+fi
 source ~/.virtualenvs/circalizer/bin/activate
-~/.virtualenvs/circalizer/bin/pip3 install --user --upgrade -r requirements.txt
+~/.virtualenvs/circalizer/bin/pip3 install --user --upgrade -r build-aux/jupyter-requirements.txt
 
 ipython3 kernel install --user --name=circalizer
